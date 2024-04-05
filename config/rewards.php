7b82dca76f8a00b90891ed7c0655524ae65fb6c5
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-04-05 18:45:52 +0200

    add rewards config file

--- config/rewards.php
+++ config/rewards.php
@@ -1,5 +1,24 @@
 <?php
 
 return [
-    //
+    /*
+    |--------------------------------------------------------------------------
+    | Reward Points Calculator
+    |--------------------------------------------------------------------------
+    |
+    | Specify which class to use for calculating reward points.
+    |
+    */
+    'reward_point_calculator' => \Dystcz\LunarRewards\Domain\Rewards\Actions\RewardPointsCalculator::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Reward Points Coefficient
+    |--------------------------------------------------------------------------
+    |
+    | Specify the coefficient to use for calculating reward points.
+    | The coefficient multiplies the order total to calculate the reward points.
+    |
+    */
+    'reward_point_coefficient' => 1,
 ];
