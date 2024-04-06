bb6036f78ca72309f95ec293a71167e2add5a71a
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-04-07 00:33:42 +0200

    add example feature test

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,10 @@
+<?php
+
+use Dystcz\LunarRewards\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(TestCase::class, RefreshDatabase::class);
+
+it('can test', function () {
+    $this->assertTrue(true);
+});
